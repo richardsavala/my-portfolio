508f1952eaefecd6821681f263ff753afb40056b
Richard <82630326+richardsavala@users.noreply.github.com>
2021-10-29 18:23:22 -0600

    updated svg and color palette

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ export default function About() {
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+              className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
             >
               Contact me
             </a>
@@ -29,7 +29,7 @@ export default function About() {
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2.w-5/6">
           <img
-            src="./coding.svg"
+            src="./coding2.svg"
             alt="hero"
             className="object-cover object-center rounded"
           />
