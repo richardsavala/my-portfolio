603de1b394e58e5d5ecbb2743b4b5950df4b9c7a
Richard <82630326+richardsavala@users.noreply.github.com>
2022-09-30 15:38:58 -0600

    updated data

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,12 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-items-center">
         <div className="lg:flex-grow md:w-1/2 ld:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi i'm Richard.
+            Hey I'm Richard.
             <br className="hidden lg:inline-block" /> I like to code!
           </h1>
           <p className="mb-8 leading-relaxed">
-            Studying web development since 2019, now seeking web development
-            career opportunities.
+            Studying web development since 2019, now seeking Web / Email
+            Development positions.
           </p>
           <div className="flex justify-center">
             <a
